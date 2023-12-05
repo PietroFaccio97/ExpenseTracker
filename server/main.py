ac6cad80952a8715c1f3343d6e1dd78337d0ed12
PietroFaccio97 <faccio.face97@gmail.com>
2023-12-05 01:10:10 +0100

    FastAPI & Uvicorn setup

--- server/main.py
+++ server/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+import uvicorn
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
+@app.get("/")
+def get_root():
+    return "Hello World!"
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("FastAPI server is running on http://127.0.0.1:8000/")
+    uvicorn.run(app, host="127.0.0.1", port=8000)
